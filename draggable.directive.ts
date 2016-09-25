50ee127c6da79b3ba2cf2fb23e8f607908afcbac
Ced c <cedvandenbosch@gmail.com>
2016-09-25 16:42:47 +0200

    readme update

--- draggable.directive.ts
+++ draggable.directive.ts
@@ -2,15 +2,17 @@ import { Observable } from 'rxjs/Rx';
 import { Subject} from 'rxjs/Subject';
 import { Directive, ElementRef, HostListener, Renderer,  EventEmitter, OnInit, OnDestroy } from '@angular/core';
 
+export interface Position{top:number, left:number}
+
 @Directive({
-  selector: '[my-draggable]'
+  selector: '[n2-draggable]'
 })
 export class Draggable implements OnInit, OnDestroy {
 
     mouseup:Observable<MouseEvent>;
     mousemove:Observable<MouseEvent>;
     mousedown:Observable<MouseEvent>;
-    mousedrag;
+    mousedrag:any;
 
     constructor(public element: ElementRef) {
       this.mouseup   = Observable.fromEvent<MouseEvent>(document, 'mouseup');
@@ -20,7 +22,8 @@ export class Draggable implements OnInit, OnDestroy {
       this.element.nativeElement.style.position = 'relative';
       this.element.nativeElement.className += ' cursor-grabbable';
 
-      this.mousedrag = this.mousedown.map((event:MouseEvent) => {
+      this.mousedrag = this.mousedown.map((event:MouseEvent):Position => {
+
           return {
             //using style instead of clientboundingrect or offsetTop because else it's buggy
               top: event.clientY - element.nativeElement.style.top.replace('px',''),
@@ -39,7 +42,7 @@ export class Draggable implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.mousedrag.subscribe({
-            next: pos => {
+            next: (pos:any) => {
                 this.element.nativeElement.style.top = pos.top + 'px';
                 this.element.nativeElement.style.left = pos.left + 'px';
             }
--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
 ### draggable
 
+[![Build Status][travis-badge]][travis-badge-url]
+[![Dependency Status][david-badge]][david-badge-url]
+[![devDependency Status][david-dev-badge]][david-dev-badge-url]
+[![npm][npm-badge]][npm-badge-url]
+
 Angular 2 directive to make an element draggable. This is different from drag
 and drop as this is used to drag a div around.
 
